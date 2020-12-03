99909e252b5c568f0ddb3dc47d01ddcde6cba225
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2020-12-03 18:36:36 +0800

    Add files via upload

---
+++ Libraries.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 18:08:17 2020
+
+@author: USER
+"""
+
+import datetime
+import dateutil
+import math
+import numpy as np
+import pandas as pd
+import random
+import string
+import time
+import sys
+sys.path.insert(0, 'C:\Document\GeneratedData')
+import GenerateData
+
+
+
+
+
+def CombineFinalDiagnosis(DiagnosisDataFrame):
+
+    
+    print("Hi, welcome to the routine.")
+    print("This routine aims at combining all final diagnoses")
+
+
+
+
+    Diagnosis_All = DischargeDiagnosis_raw[['AdmissionID','DiagnosisCode0', 'DiagnosisCode1', 'DiagnosisCode2', 'DiagnosisCode3',
+                                            'DiagnosisCode4', 'DiagnosisCode5', 'DiagnosisCode6', 'DiagnosisCode7', 'DiagnosisCode8', 'DiagnosisCode9']]
+
+    print("Done. Start stacking diagnosis results..")
+
+    list_out_diagnosis = []
+    Diagnosis_Stacked = pd.DataFrame(columns=['AdmissionID', 'DiagnosisOrder', 'DiagnosisCode' ])
+
+    #Extract each diagnosis as seperate dataframe and concatenate all dataframes.
+    for i in range(10): #total of 10 diagnosis(maximum, rangning from 0 to 9)
+        
+        clmn_temp = 'DiagnosisCode'+str(i)
+        list_out_diagnosis.append(clmn_temp)
+   
+        df_temp = Diagnosis_All[['AdmissionID','DiagnosisCode'+str(i)]]
+        df_temp['DiagnosisOrder'] = i
+        df_temp = df_temp[['AdmissionID','DiagnosisOrder', 'DiagnosisCode'+str(i)]]
+        df_temp.columns = ['AdmissionID', 'DiagnosisOrder', 'DiagnosisCode' ]
+        Diagnosis_Stacked = pd.concat([Diagnosis_Stacked, df_temp])
+        Diagnosis_Stacked['DiagnosisCode'].replace('', np.nan, inplace=True)#Replace space with Null
+
+
+        Diagnosis_Stacked= Diagnosis_Stacked.dropna(subset=['DiagnosisCode'])#Drop Null value
+
+    return Diagnosis_Stacked
+
+DiagnosisStacked = CombineFinalDiagnosis(DischargeDiagnosis_raw)
+
+print(DiagnosisStacked.head(10))
