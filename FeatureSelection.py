83b9aa8af3a350f866502c0ab9ace7513f3d1389
Yun-Chung LIU <46064664+halfmoonliu@users.noreply.github.com>
2020-12-14 19:26:30 +0800

    Update FeatureSelection.py

--- FeatureSelection.py
+++ FeatureSelection.py
@@ -1,26 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 14 16:56:24 2020
-
-@author: USER
-"""
-
 
 import pandas as pd
 
-
-Cohort_raw = pd.read_excel(r'C:\Document\GeneratedData\Cohort_ICU24H.xlsx', header =0)
-
-ComorbidConditions_raw = pd.read_excel(r'C:\Document\GeneratedData\ComorbidConditions.xlsx', header =0)
-LabExamOneHot_raw = pd.read_excel(r'C:\Document\GeneratedData\ExamOneHot.xlsx', header =0)
-
-VitalSignOneHot_raw = pd.read_excel(r'C:\Document\GeneratedData\VitalSignOneHot.xlsx', header =0)
-
 def FeatureSelection(Cohort, ComorbidCondition, LabExam, VitalSign):
+    '''
+    This function takes the following four dataframes:
+    1. Cohort: Cohort with label of ICU admission in the first day.
+    2. ComorbidCondition: Patitent data of their comorbid conditions.
+    3. LabExam: Patitent initial exam results within admission -3~+1 days.
+    4. VitalSign: Patitent vital sign data preprocessed, recorded within admission +/- 1 day.
+    
+    The function outputs a dataframe of AdmissionID, Group (prediction taget, transferred into ICU or not within the first day of admission) and selected features.
+    '''
     
     Dataset = Cohort[['AdmissionID', 'FirstDayICU', 'Age', 'Sex_int']]
-    
-    
+  
     
     #Comorbid Conditions: 'ComorbidCondition1', 'ComorbidCondition2', 'ComorbidCondition3', 'ComorbidCondition4', 'ComorbidCondition5'
     
@@ -41,7 +34,3 @@ def FeatureSelection(Cohort, ComorbidCondition, LabExam, VitalSign):
     
     
     return Dataset
-
-results = FeatureSelection(Cohort_raw, ComorbidConditions_raw, LabExamOneHot_raw, VitalSignOneHot_raw)
-
-results.to_excel(r"C:\Document\GeneratedData\Dataset.xlsx", sheet_name='Sheet1', index = False) 
